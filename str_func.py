68191148ee4eb6aa6005ef535c9501f1ea070650
ArtemV <temanbi4@gmail.com>
2023-05-23 18:23:06 +0300

    add str_gunc.py with func of Upper and add def capitalize

--- str_func.py
+++ str_func.py
@@ -1,3 +1,18 @@
 def upper_string(input_string):
     """convert string to Upper"""
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """
+    Capitalizes the first letter of each word in the input string.
+
+    Args:
+        input_string (str): The input string.
+
+    Returns:
+        str: The input string with the first letter of each word capitalized.
+    """
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return " ".join(capitalized_words)
